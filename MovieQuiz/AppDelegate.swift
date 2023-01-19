115c58e47aafa48788fd5def9cb12887e161f9ab
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-01-19 14:06:01 +0600

    Remove comments

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -1,20 +1,13 @@
-//
-//  AppDelegate.swift
-//  MovieQuiz
-//
-//  Created by Constantine Nikolsky on 15.05.2022.
-//
-
 import UIKit
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
+        true
     }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
